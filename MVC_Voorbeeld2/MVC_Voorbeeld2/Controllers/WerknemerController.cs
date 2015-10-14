49a23f8eb2d19a2eeb4f0f1cd95778df4b2d6910
Kurt Kreemers <kkreeme1@gmail.com>
2015-10-14 14:15:54 +0200

    13102015

---
+++ MVC_Voorbeeld2/MVC_Voorbeeld2/Controllers/WerknemerController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Voorbeeld2.Controllers
+{
+    public class WerknemerController : Controller
+    {
+        // GET: Werknemer
+        public ActionResult Index()
+        {
+            return View("AndereNaam");
+        }
+        [NonAction]
+        public void GeenAction()
+        {
+
+        }
+        [HttpPost]
+        public ActionResult VerdubbelDeWeddes()
+        {
+            return View();
+        }
+    }
+}
