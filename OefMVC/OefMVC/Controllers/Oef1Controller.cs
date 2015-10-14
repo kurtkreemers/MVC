c7f06ae77b019d0d044ed807cf666b44c357128e
Kurt Kreemers <kkreeme1@gmail.com>
2015-10-14 14:50:42 +0200

    13102015

---
+++ OefMVC/OefMVC/Controllers/Oef1Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OefMVC.Controllers
+{
+    public class Oef1Controller : Controller
+    {
+        // GET: Oef1
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ OefMVC/OefMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OefMVC.MvcApplication" Language="C#" %>
