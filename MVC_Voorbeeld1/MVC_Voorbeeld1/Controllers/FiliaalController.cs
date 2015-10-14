fd102275ec0cd73d3415be91bc686f7fb3e89a21
Kurt Kreemers <kkreeme1@gmail.com>
2015-10-14 13:45:03 +0200

    13102015

---
+++ MVC_Voorbeeld1/MVC_Voorbeeld1/Controllers/FiliaalController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Voorbeeld1.Controllers
+{
+    public class FiliaalController : Controller
+    {
+        // GET: Filiaal
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Show()
+        {
+            return View();
+        }
+        public ActionResult Read(int? id)
+        {
+            return View();
+        }
+    }
+}
