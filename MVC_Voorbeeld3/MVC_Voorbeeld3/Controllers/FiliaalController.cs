06e69dfc97a85086060ccba122a525315989b9f7
Kurt Kreemers <kkreeme1@gmail.com>
2015-10-15 13:19:08 +0200

    hfst7

---
+++ MVC_Voorbeeld3/MVC_Voorbeeld3/Controllers/FiliaalController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_Voorbeeld3.Services;
+
+namespace MVC_Voorbeeld3.Controllers
+{
+    
+    public class FiliaalController : Controller
+    {
+        private FiliaalService filiaalservice = new FiliaalService();
+        private HoofdZetelService hoofdZetelService = new HoofdZetelService();
+        // GET: Filiaal
+        public ActionResult Index()
+        {
+            var hoofdZetel = hoofdZetelService.Read();
+            ViewBag.hoofdZetel = hoofdZetel;
+            var filialen = filiaalservice.FindAll();
+            return View(filialen);
+        }
+    }
+}
---
+++ MVC_Voorbeeld3/MVC_Voorbeeld3/Models/HoofdZetel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Voorbeeld3.Models
+{
+    public class HoofdZetel
+    {
+        public string Straat { get; set; }
+        public string HuisNr { get; set; }
+        public string PostCode { get; set; }
+        public string Gemeente { get; set; }
+    }
+}
