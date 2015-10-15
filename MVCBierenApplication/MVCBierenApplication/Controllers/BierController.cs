33e7fcad966dd2f001859e24ef72e7114e8a1d8c
Kurt Kreemers <kkreeme1@gmail.com>
2015-10-15 09:49:03 +0200

    15102015

---
+++ MVCBierenApplication/MVCBierenApplication/Controllers/BierController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCBierenApplication.Models;
+
+namespace MVCBierenApplication.Controllers
+{
+    public class BierController : Controller
+    {
+        // GET: Bier
+        public ActionResult Index()
+        {
+            var bieren = new List<Bier>();
+
+            bieren.Add(new Bier { ID = 1, Naam = "Jupiler", Alcohol = 0.8f });
+            bieren.Add(new Bier { ID = 2, Naam = "Leffe", Alcohol = 1.2f });
+            bieren.Add(new Bier { ID = 3, Naam = "Hoegaarden", Alcohol = 2.3f });
+
+            return View(bieren);
+        }
+    }
+}
---
+++ MVCBierenApplication/MVCBierenApplication/Models/Bier.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCBierenApplication.Models
+{
+    public class Bier
+    {
+        public int ID { get; set; }
+        public string Naam { get; set; }
+        public float Alcohol { get; set; }
+    }
+}
