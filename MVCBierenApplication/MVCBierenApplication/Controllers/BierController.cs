60dc6377242abe45c8b3d8a2762680ec9ba0aaa7
Kurt Kreemers <kkreeme1@gmail.com>
2015-10-16 10:52:34 +0200

    oef hfst9

--- MVCBierenApplication/MVCBierenApplication/Controllers/BierController.cs
+++ MVCBierenApplication/MVCBierenApplication/Controllers/BierController.cs
@@ -15,8 +15,9 @@ namespace MVCBierenApplication.Controllers
             var bieren = new List<Bier>();
 
             bieren.Add(new Bier { ID = 1, Naam = "Jupiler", Alcohol = 0.8f });
-            bieren.Add(new Bier { ID = 2, Naam = "Leffe", Alcohol = 1.2f });
-            bieren.Add(new Bier { ID = 3, Naam = "Hoegaarden", Alcohol = 2.3f });
+            bieren.Add(new Bier { ID = 2, Naam = "Leffe", Alcohol = 2.2f });
+            bieren.Add(new Bier { ID = 3, Naam = "Hoegaarden", Alcohol = 5.3f });
+            bieren.Add(new Bier { ID = 4, Naam = "Stella Artois", Alcohol = 8.3f });
 
             return View(bieren);
         }
