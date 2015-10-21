7f185d1cd2423b9eff9fa16add94f3e49b695404
Kurt Kreemers <kkreeme1@gmail.com>
2015-10-21 10:20:38 +0200

    oef hfst 11

--- MVCBierenApplication/MVCBierenApplication/Controllers/BierController.cs
+++ MVCBierenApplication/MVCBierenApplication/Controllers/BierController.cs
@@ -4,46 +4,41 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using MVCBierenApplication.Models;
+using MVCBierenApplication.Services;
 
 namespace MVCBierenApplication.Controllers
 {
     public class BierController : Controller
     {
-        private static Dictionary<int, Bier> bierenDictionary = new Dictionary<int, Bier>();
+        private BierService Bieren = new BierService();
+       
         // GET: Bier
         public ActionResult Index()
-        {
-            var bieren = new List<Bier>();
+        {          
+            var bierenLijst = Bieren.FindAll();
+            return View(bierenLijst);
 
-            bieren.Add(new Bier { ID = 1, Naam = "Jupiler", Alcohol = 0.8f });
-            bieren.Add(new Bier { ID = 2, Naam = "Leffe", Alcohol = 2.2f });
-            bieren.Add(new Bier { ID = 3, Naam = "Hoegaarden", Alcohol = 5.3f });
-            bieren.Add(new Bier { ID = 4, Naam = "Stella Artois", Alcohol = 8.3f });
-
-            return View(bieren);
+           
         }
         public ActionResult Verwijderen(int id)
         {
-            var bier = Read(id);
-
+            var bier = Bieren.Read(id);
             return View(bier);
         }
+        [HttpPost]
         public ActionResult DeleteView(int id)
         {
-            var bier = Read(id);
+            var bier = Bieren.Read(id);
             this.TempData["bier"] = bier;
-            Delete(id);
-            return Redirect("~/Bier/Verwijderd");
+            Bieren.Delete(id);
+            return Redirect("~/Bier/verwijderd");
         }
-        public Bier Read(int id)
-        {
-            return bierenDictionary[id];
-        }
+       
 
-        public void Delete(int id)
+        public ActionResult verwijderd()
         {
-          bierenDictionary.Remove(id);
+            var bier = (Bier)TempData["Bier"];
+            return View(bier);
         }
-
     }
 }
--- MVCBierenApplication/MVCBierenApplication/Services/BierService.cs
+++ MVCBierenApplication/MVCBierenApplication/Services/BierService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MVCBierenApplication.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,5 +8,27 @@ namespace MVCBierenApplication.Services
 {
     public class BierService
     {
+        private static Dictionary<int, Bier> bierenDictionary = new Dictionary<int, Bier>();
+
+        static BierService()
+        {
+            bierenDictionary[1] = new Bier { ID = 1, Naam = "Jupiler", Alcohol = 0.8f };
+            bierenDictionary[2] = new Bier { ID = 2, Naam = "Leffe", Alcohol = 2.2f };
+            bierenDictionary[3] = new Bier { ID = 3, Naam = "Hoegaarden", Alcohol = 5.3f };
+            bierenDictionary[4] = new Bier { ID = 4, Naam = "Stella Artois", Alcohol = 8.3f };
+        }
+        public Bier Read(int id)
+        {
+            return bierenDictionary[id];
+        }
+
+        public void Delete(int id)
+        {
+            bierenDictionary.Remove(id);
+        }
+        public List<Bier> FindAll()
+        {
+            return bierenDictionary.Values.ToList();
+        }
     }
 }
