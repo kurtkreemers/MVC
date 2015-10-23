e207f112d9759a419795c51d28876cace87148c5
Kurt Kreemers <kkreeme1@gmail.com>
2015-10-23 12:12:15 +0200

    hfst 14

---
+++ MVC_Tuincentrum/MVC_Tuincentrum/Controllers/LeveranciersController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MVC_Tuincentrum.Models;
+
+namespace MVC_Tuincentrum.Controllers
+{
+    public class LeveranciersController : Controller
+    {
+        private TuincentrumEntities db = new TuincentrumEntities();
+
+        // GET: Leveranciers
+        public ActionResult Index()
+        {
+            return View(db.Leveranciers.ToList());
+        }
+
+        // GET: Leveranciers/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Leverancier leverancier = db.Leveranciers.Find(id);
+            if (leverancier == null)
+            {
+                return HttpNotFound();
+            }
+            return View(leverancier);
+        }
+
+        // GET: Leveranciers/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Leveranciers/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "LevNr,Naam,Adres,PostNr,Woonplaats,Versie")] Leverancier leverancier)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Leveranciers.Add(leverancier);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(leverancier);
+        }
+
+        // GET: Leveranciers/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Leverancier leverancier = db.Leveranciers.Find(id);
+            if (leverancier == null)
+            {
+                return HttpNotFound();
+            }
+            return View(leverancier);
+        }
+
+        // POST: Leveranciers/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "LevNr,Naam,Adres,PostNr,Woonplaats,Versie")] Leverancier leverancier)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(leverancier).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(leverancier);
+        }
+
+        // GET: Leveranciers/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Leverancier leverancier = db.Leveranciers.Find(id);
+            if (leverancier == null)
+            {
+                return HttpNotFound();
+            }
+            return View(leverancier);
+        }
+
+        // POST: Leveranciers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Leverancier leverancier = db.Leveranciers.Find(id);
+            db.Leveranciers.Remove(leverancier);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
