076f506708711df278f619e30f82b14d9d560209
Kurt Kreemers <kkreeme1@gmail.com>
2015-10-23 12:11:31 +0200

    oef hfst 14

---
+++ MVCBierenApplication/MVCBierenApplication/Models/BierProperties.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCBierenApplication.Models
+{
+    public class BierProperties
+    {
+        [DisplayFormat(DataFormatString = "{0:000}")]
+        public int BierNr { get; set; }
+        [Required]
+        [StringLength(20, ErrorMessage = "Max. {1} tekens voor {0}")]
+        public string Naam { get; set; }
+        [UIHint("kleuren")]
+        [AlcoholValueControl(ErrorMessage = "{0} heeft een ongeldige waarde")]
+        public float Alcohol { get; set; }
+    }
+}
--- MVCBierenApplication/MVCBierenApplication/Services/BierService.cs
+++ MVCBierenApplication/MVCBierenApplication/Services/BierService.cs
@@ -8,32 +8,41 @@ namespace MVCBierenApplication.Services
 {
     public class BierService
     {
-        private static Dictionary<int, Bier> bierenDictionary = new Dictionary<int, Bier>();
 
         static BierService()
         {
-            bierenDictionary[1] = new Bier { ID = 1, Naam = "Jupiler", Alcohol = 0.8f };
-            bierenDictionary[2] = new Bier { ID = 2, Naam = "Leffe", Alcohol = 2.2f };
-            bierenDictionary[3] = new Bier { ID = 3, Naam = "Hoegaarden", Alcohol = 5.3f };
-            bierenDictionary[4] = new Bier { ID = 4, Naam = "Stella Artois", Alcohol = 8.3f };
         }
         public Bier Read(int id)
         {
-            return bierenDictionary[id];
+            using (var db = new MVCBierenEntities())
+            {
+                return db.Bieren.Find(id);
+            }
         }
 
         public void Delete(int id)
         {
-            bierenDictionary.Remove(id);
+            using (var db = new MVCBierenEntities())
+            {
+                Bier bier = db.Bieren.Find(id);
+                db.Bieren.Remove(bier);
+                db.SaveChanges();
+            }
         }
         public List<Bier> FindAll()
         {
-            return bierenDictionary.Values.ToList();
+            using (var db = new MVCBierenEntities())
+            {
+                return db.Bieren.OrderBy(m => m.BierNr).ToList();
+            }
         }
         public void Add(Bier b)
         {
-            b.ID = bierenDictionary.Keys.Max() + 1;
-            bierenDictionary.Add(b.ID, b);
+            using (var db = new MVCBierenEntities())
+            {
+                db.Bieren.Add(b);
+                db.SaveChanges();
+            }
         }
     }
 
