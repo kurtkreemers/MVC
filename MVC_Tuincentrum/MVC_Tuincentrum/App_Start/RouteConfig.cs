a89cec90c1f63d9f65f07fdb1015b4e788f9f57c
Kurt Kreemers <kkreeme1@gmail.com>
2015-10-28 16:01:38 +0100

    p130

--- MVC_Tuincentrum/MVC_Tuincentrum/App_Start/RouteConfig.cs
+++ MVC_Tuincentrum/MVC_Tuincentrum/App_Start/RouteConfig.cs
@@ -14,10 +14,20 @@ namespace MVC_Tuincentrum
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                "Alleplanten",
+                "Plantenlijst",
+                new { controller = "Plant", action = "Index" });
+
+            routes.MapRoute(
+                "PlantByNr",
+                "Plant/{id}",
+                new { controller = "Plant", action = "Details" });
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional });
+
         }
     }
 }
